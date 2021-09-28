f7ca7cdeaeac97838949c726cc4a0804c03bc446
radeen <radeen013@gmail.com>
2021-09-28 13:47:56 -0400

    first commit

---
+++ Day2Exercise/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Day2Exercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Mini exercise practicing loops
+
+            for (int i = 0; i <= 100; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            
+            }
+
+            
+        }
+    }
+}
